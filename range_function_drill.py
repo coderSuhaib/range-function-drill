08dcc911afcbd1e788104d321174e1a53c84f2db
Suhaib Al-Tamimi <suhaib.altamimi3@gmail.com>
2017-08-07 14:35:49 -0700

    This repository it to show my knowledge on how to use the range function in python

---
+++ range_function_drill.py
@@ -0,0 +1,19 @@
+
+myNum = [1,2,3,4,5,6,7,8,9,10]
+
+# 1. range() function with one parameter to display the following 0 1 2 3
+for i in range(4):
+    print (i)
+
+print ( )
+# 2. use the python range() function with 3 parameters to display the following 3 2 1 0
+
+for y in range(3,-1,-1):
+    print (y)
+
+print ( )
+
+# 3. Use the python range() function with 3 parameters to sidplay the follwoing 8 6 4 2
+
+for x in range(8,1,-2):
+    print (x)
